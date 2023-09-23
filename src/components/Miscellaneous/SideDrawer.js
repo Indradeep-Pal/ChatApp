27ebd04e0f8c2afe7fe5ed68a702b0f417818357
Indradeep Pal <124890968+Indradeep-Pal@users.noreply.github.com>
2023-09-23 18:24:11 +0530

    Update SideDrawer.js

--- src/components/Miscellaneous/SideDrawer.js
+++ src/components/Miscellaneous/SideDrawer.js
@@ -60,7 +60,7 @@ const SideDrawer = () => {
                 },
             };
 
-            const {data}=await axios.get(`/api/user?search=${search}`,config);
+            const {data}=await axios.get(`https://webappp.onrender.com/api/user?search=${search}`,config);
             setLoading(false);
             setsearchResult(data); 
         } catch (error) {
@@ -86,7 +86,7 @@ const SideDrawer = () => {
                 },
             };
 
-            const {data} = await axios.post('/api/chat',{userId},config);
+            const {data} = await axios.post('https://webappp.onrender.com/api/chat',{userId},config);
             
             if(!chats.find((c)=> c.id===data.id))
                 setChats([data,...chats])
